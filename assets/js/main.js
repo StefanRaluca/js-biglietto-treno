da8acf818a03ab62d1fd91e528990f0e908ccef0
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-19 16:33:34 +0100

    add total price

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,33 @@
 //Richiedere all'utente tramite input i 2 valori (prima i km e dopo la sua età)
 //Stampiamo in console i 2 valori 
 
-let km_number = prompt("how many km will you travel?");
+let km_number = Number(prompt("how many km will you travel?"));
 console.log(km_number);
-let age_user = prompt("how old will you be when you travel?");
+let age_user = Number(prompt("how old will you be when you travel?"));
 console.log(age_user);
-  
+
 //Stabiliamo il prezzo del biglietto con una variabile
-let price_km=0.21;
-console.log(price_km);
+let price_km = 0.21;
+let discount_child = 0.2;
+let discount_senior = 0.4;
+
+//Eseguiamo delle operazioni math:
+if (age_user < 18) {
+    let price_passenger = (km_number * price_km);
+    let price_total = (price_passenger / discount_child)
+    console.log(price_total);
+
+}
+
+else if (age_user > 18 && age_user > 65)  {
+    let price_passenger = (km_number * price_km);
+    let price_total = (price_passenger / discount_senior)
+    console.log(price_total);
+}
+
+else {
+    let price_total = (km_number * price_km);
+    console.log(price_total);
+}
+
 
